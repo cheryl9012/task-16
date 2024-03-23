f153df47dc11716bf408c3b8fce41c6373e859de
cheryl9012 <161365360+cheryl9012@users.noreply.github.com>
2024-03-23 21:07:58 +0500

    Add files via upload
    In this task, more people were added to the list of dinner invitation.

---
+++ Q16.js
@@ -0,0 +1,11 @@
+"use strict";
+// More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+// • Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+// • Add one new guest to the beginning of your array.
+// • Add one new guest to the middle of your array. • Use append() to add one new guest to the end of your list. • Print a new set of invitation messages, one for each person in your list.
+let guest = ["Jennifer", "Anum", "Aster", "Noman", "Sophia", "gillian", "Ruth"];
+let canNotAttend = "Noman";
+let newguest = "Munir";
+guest[guest.indexOf(canNotAttend)] = newguest;
+console.log(guest);
+guest.map((items) => console.log(`Dear ${items}, I found a bigger dinner table `));
